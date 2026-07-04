ecfbb0d81c8449c9489f9ea695aac1422d2baa2b
komi-juru <komi-juru@users.noreply.github.com>
2026-07-04 21:41:36 +0900

    fix: restore missing worker imports in HeaderBarIcon for manual translation button

--- components/HeaderBarIcon.tsx
+++ components/HeaderBarIcon.tsx
@@ -12,6 +12,7 @@ import { Menu, Popout, SelectedChannelStore, Toasts,useEffect, useRef, useState,
 // Removed getDeepLUsage import
 import { getDeepSeekBalance } from "../api/deepseek";
 import { reScheduleAllWorkers, resetTranslationQueues } from "../api/translate";
+import { deeplWorkers, geminiWorkers } from "../api/worker";
 import { CHANNEL_ENGINE_OPTIONS, GLOBAL_ENGINE_OPTIONS, LANGUAGES } from "../constants";
 import { addChannel, removeChannel,settings, updateChannel } from "../settings";
 import { TranslationCache } from "../utils/cache";
